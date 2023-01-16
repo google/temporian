ccbd86c09107a200c0aea9c0e32fd9dcc3c4923f
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-01-16 12:58:43 -0300

    Adding operator imports

--- temporal_feature_processor/implementation/pandas/operators/window/__init__.py
+++ temporal_feature_processor/implementation/pandas/operators/window/__init__.py
@@ -12,3 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from temporal_feature_processor.implementation.pandas.operators.window.base import PandasWindowOperator
+from temporal_feature_processor.implementation.pandas.operators.window.simple_moving_average import PandasSimpleMovingAverageOperator
