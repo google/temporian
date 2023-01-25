975b720b459ccce6638db48af2fe6c6796fed696
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-01-25 16:49:47 -0300

    Removing output list from pandas evaluator

--- temporal_feature_processor/implementation/pandas/evaluator.py
+++ temporal_feature_processor/implementation/pandas/evaluator.py
@@ -10,9 +10,8 @@ def evaluate_schedule(
     data: Dict[event.Event,
                pandas_event.PandasEvent], schedule: List[base.Operator]
 ) -> Dict[event.Event, pandas_event.PandasEvent]:
-  outputs = []
-  for operator in schedule:
 
+  for operator in schedule:
     # get implementation
     implementation = core_mapping.OPERATOR_IMPLEMENTATIONS[operator.definition(
     ).key]()  # TODO: add operator attributes when instancing implementation
@@ -25,10 +24,9 @@ def evaluate_schedule(
 
     # compute output
     operator_outputs = implementation(**operator_inputs)
-    outputs.append(operator_outputs)
 
     # materialize data in output events
     for output_key, output_event in operator.outputs().items():
       data[output_event] = operator_outputs[output_key]
 
-  return outputs
+  return data
