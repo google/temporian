021784b14fbd1eb600b76b8f391040cfe5538d86
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-02-07 14:48:08 -0300

    Adding NumPy backend

---
+++ temporian/implementation/numpy/data/event.py
@@ -0,0 +1,50 @@
+from typing import Dict, List, Tuple
+
+import numpy as np
+
+from temporian.core.data.event import Event
+from temporian.core.data.feature import Feature
+from temporian.implementation.numpy.data.sampling import NumpySampling
+
+
+class NumpyFeature:
+    def __init__(self, name: str, index: List[str], data: np.ndarray) -> None:
+        if len(data.shape) > 1:
+            raise ValueError(
+                "NumpyFeatures can only be created from flat arrays. Passed"
+                f" input's shape: {len(data.shape)}"
+            )
+
+        self.name = name
+        self.index = index
+        self.data = data
+        self.dtype = self.data.dtype
+
+    def schema(self) -> Feature:
+        return Feature(
+            name=self.name, dtype=self.dtype, sampling=self.index.names
+        )
+
+    def __repr__(self) -> str:
+        return f"{self.name}: {self.data.__repr__()}"
+
+
+class NumpyEvent:
+    def __init__(
+        self,
+        data: Dict[Tuple, List[NumpyFeature]],
+        sampling: NumpySampling,
+    ) -> None:
+        self.data = data
+        self.sampling = sampling
+
+    def schema(self) -> Event:
+        return Event(
+            features=[
+                feature.schema() for feature in list(self.data.values())[0]
+            ],
+            sampling=self.sampling.names,
+        )
+
+    def __repr__(self) -> str:
+        return self.data.__repr__()
---
+++ temporian/implementation/numpy/operators/tests/assign/test_data/with_idx_more_timestamps.py
@@ -0,0 +1,85 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""PandasAssignOperator - with index, more timestamps test.
+
+Tests the correct output when the assigned event has more timestamps than the assignee 
+event, for any index value. Both input events are indexed.
+"""
+
+import pandas as pd
+
+from temporian.implementation.pandas.data.event import PandasEvent
+
+INPUT_1 = PandasEvent(
+    {
+        "product_id": [
+            666964,
+            372306,
+        ],
+        "timestamp": [
+            pd.Timestamp("2013-01-01"),
+            pd.Timestamp("2013-01-05"),
+        ],
+        "sales": [
+            0.0,
+            1160.0,
+        ],
+    }
+).set_index(["product_id", "timestamp"])
+
+INPUT_2 = PandasEvent(
+    {
+        "product_id": [
+            666964,
+            372306,
+            372306,
+        ],
+        "timestamp": [
+            pd.Timestamp("2013-01-01"),
+            pd.Timestamp("2013-01-05"),
+            pd.Timestamp(
+                "2013-01-07"
+            ),  # more timestamps than assignee event for porudct_id = 372306
+        ],
+        "costs": [
+            0.0,
+            508.0,
+            573.0,
+        ],
+    }
+).set_index(["product_id", "timestamp"])
+
+OUTPUT = PandasEvent(
+    {
+        "product_id": [
+            666964,
+            372306,
+        ],
+        "timestamp": [
+            pd.Timestamp("2013-01-01"),
+            pd.Timestamp(
+                "2013-01-05"
+            ),  # assignee event's timestamps are preserved
+        ],
+        "sales": [
+            0.0,
+            1160.0,
+        ],
+        "costs": [
+            0.0,
+            508.0,
+        ],
+    }
+).set_index(["product_id", "timestamp"])
