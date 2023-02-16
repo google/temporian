ab396bef6b230a7e37a18264ceb758df242a6620
Diego Marvid <dmarvid@tryolabs.com>
2023-02-16 12:14:41 -0300

    changed lag implementation & dropped period

--- temporian/implementation/numpy/operators/tests/lag/lag_test.py
+++ temporian/implementation/numpy/operators/tests/lag/lag_test.py
@@ -16,36 +16,22 @@ from absl.testing import absltest
 
 from temporian.implementation.numpy.operators import lag
 from temporian.implementation.numpy.operators.tests.lag.test_data import (
-    correct_duration_lag,
-)
-from temporian.implementation.numpy.operators.tests.lag.test_data import (
-    correct_period_lag,
+    correct_lag,
 )
 
 
 class SumOperatorTest(absltest.TestCase):
     def setUp(self) -> None:
-        self.duration_operator = lag.NumpyLagOperator(duration=2)
-        self.period_operator = lag.NumpyLagOperator(period=2)
+        self.operator = lag.NumpyLagOperator(duration=2)
 
-    def test_correct_duration_lag(self) -> None:
-        operator_output = self.duration_operator(
-            correct_duration_lag.INPUT,
+    def test_correct_lag(self) -> None:
+        operator_output = self.operator(
+            correct_lag.INPUT,
         )
 
         self.assertEqual(
             True,
-            correct_duration_lag.OUTPUT == operator_output["event"],
-        )
-
-    def test_correct_period_lag(self) -> None:
-        operator_output = self.period_operator(
-            correct_period_lag.INPUT,
-        )
-
-        self.assertEqual(
-            True,
-            correct_period_lag.OUTPUT == operator_output["event"],
+            correct_lag.OUTPUT == operator_output["event"],
         )
 
 
