4d9108263d5d459d4f5cf900effe5aa0a9cc628b
Diego Marvid <dmarvid@tryolabs.com>
2023-02-16 12:59:39 -0300

    removed duration and names for rebase

--- temporian/implementation/numpy/operators/lag.py
+++ temporian/implementation/numpy/operators/lag.py
@@ -1,22 +1,21 @@
 from temporian.implementation.numpy.data.event import NumpyEvent
 from temporian.implementation.numpy.data.sampling import NumpySampling
-from temporian.core.data.duration import Duration
 
 
 class NumpyLagOperator:
-    def __init__(self, duration: Duration) -> None:
+    def __init__(self, duration: float) -> None:
         super().__init__()
         self.duration = duration
 
     def __call__(self, event: NumpyEvent) -> NumpyEvent:
         new_sampling = NumpySampling(
             data={},
-            index=event.sampling.index,
+            names=event.sampling.names,
         )
         output = NumpyEvent(data=event.data, sampling=new_sampling)
 
         for index, timestamps in event.sampling.data.items():
-            new_sampling.data[index] = timestamps - self.duration
+            new_sampling.data[index] = timestamps + self.duration
             for feature in output.data[index]:
                 feature.name = f"lag_{feature.name}"
 
