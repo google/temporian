3c2cc1789a0d4e1afea05de5364351bed567a0a1
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-02-16 19:34:53 -0300

    Improving reindex subset implementation

--- temporian/implementation/numpy/operators/reindex.py
+++ temporian/implementation/numpy/operators/reindex.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import numpy as np
 
@@ -7,6 +7,128 @@ from temporian.core.operators.reindex import ReIndex
 from temporian.implementation.numpy.data.event import NumpyEvent
 from temporian.implementation.numpy.data.event import NumpyFeature
 from temporian.implementation.numpy.data.sampling import NumpySampling
+
+
+def _subset_impl(
+    src_event: NumpyEvent,
+    src_idx_names: List[str],
+    dst_idx_names: List[str],
+):
+    # source sampling data
+    src_samp_data = src_event.sampling.data
+
+    # index positions to keep
+    keep_idx_pos = [
+        pos
+        for pos, index_name in enumerate(src_idx_names)
+        if index_name in dst_idx_names
+    ]
+    # index positions to drop
+    drop_idx_pos = [
+        pos
+        for pos, index_name in enumerate(src_idx_names)
+        if index_name not in dst_idx_names
+    ]
+    # dropped from index feature names
+    drop_feat_names = [src_idx_names[i] for i in drop_idx_pos]
+
+    # intialize empty dict mapping destination index levels to block lengths,
+    # features, and timestamps
+    dst_idx_metadata: Dict[
+        tuple, Dict[str, int | List[NumpyFeature] | np.array]
+    ] = {}
+
+    # loop over source index levels gathering destination index metadata
+    for src_idx_lvl, timestamps in src_samp_data.items():
+        # destination index level
+        dst_idx_lvl = tuple((src_idx_lvl[i] for i in keep_idx_pos))
+
+        # use try - except here. Could instead check if key already exists in dict,
+        # but it's likely much slower
+        try:
+            # number of samples in this index level
+            this_block_length = len(timestamps)
+
+            # collapse index level
+            drop_feats = [
+                NumpyFeature(
+                    feat_name, np.array([src_idx_lvl[i]] * this_block_length)
+                )
+                for feat_name, i in zip(drop_feat_names, drop_idx_pos)
+            ]
+            # store metadata
+            dst_idx_metadata[dst_idx_lvl]["block_length"] += this_block_length
+            dst_idx_metadata[dst_idx_lvl]["timestamps"].append(timestamps)
+            dst_idx_metadata[dst_idx_lvl]["features"].append(
+                drop_feats + src_event.data[src_idx_lvl]
+            )
+
+        except KeyError:
+            # first time destination index level is encountered - create entry
+            dst_idx_metadata[dst_idx_lvl] = {}
+            dst_idx_metadata[dst_idx_lvl]["block_length"] = this_block_length
+            dst_idx_metadata[dst_idx_lvl]["timestamps"] = [timestamps]
+            dst_idx_metadata[dst_idx_lvl]["features"] = [
+                drop_feats + src_event.data[src_idx_lvl]
+            ]
+
+    # allocate memory for destination sampling & event
+    # sampling
+    dst_samp_data = {
+        idx_lvl: np.empty(metadata["block_length"])
+        for idx_lvl, metadata in dst_idx_metadata.items()
+    }
+    # destination feature names
+    dst_feat_names = drop_feat_names + src_event.feature_names
+
+    # event
+    dst_event_data = {
+        idx_lvl: [
+            NumpyFeature(
+                name=dst_feat_name, data=np.empty(metadata["block_length"])
+            )
+            for dst_feat_name in dst_feat_names
+        ]
+        for idx_lvl, metadata in dst_idx_metadata.items()
+    }
+    # assign data to previously allocated memory
+    for dst_idx_lvl, metadata in dst_idx_metadata.items():
+        ptr = 0
+        for timestamps, feats in zip(
+            metadata["timestamps"], metadata["features"]
+        ):
+            # number of samples in this index level
+            this_block_length = len(timestamps)
+
+            # allocate sampling
+            dst_samp_data[dst_idx_lvl][
+                ptr : ptr + this_block_length
+            ] = timestamps
+
+            # allocate event
+            for i, feat in enumerate(feats):
+                dst_event_data[dst_idx_lvl][i].data[
+                    ptr : ptr + this_block_length
+                ] = feat.data
+
+            # increment pointer
+            ptr += this_block_length
+
+    # finally, sort according to timestamps
+    for dst_idx_lvl in dst_event_data.keys():
+        sorted_idxs = np.argsort(dst_samp_data[dst_idx_lvl], kind="mergesort")
+
+        # sampling
+        dst_samp_data[dst_idx_lvl] = dst_samp_data[dst_idx_lvl][sorted_idxs]
+
+        # event
+        for feature in dst_event_data[dst_idx_lvl]:
+            feature.data = feature.data[sorted_idxs]
+
+    # create & return output NumpyEvent w/ it's NumpySampling
+    return NumpyEvent(
+        dst_event_data, NumpySampling(src_idx_names, dst_samp_data)
+    )
 
 
 class ReIndexNumpyImplementation:
@@ -18,104 +140,14 @@ class ReIndexNumpyImplementation:
 
     def __call__(self, event: NumpyEvent) -> Dict[str, NumpyEvent]:
         # sampling index names
-        src_index = event.sampling.names
-        dst_index = self._op.attributes()["dst_index"]
+        src_index_names = event.sampling.names
+        dst_index_names = self._op.attributes()["dst_index"]
 
-        # index positions to keep
-        dst_index_positions = [
-            pos
-            for pos, index_name in enumerate(event.sampling.names)
-            if index_name in dst_index
-        ]
-        # index positions to collapse
-        src_index_positions = [
-            pos
-            for pos, index_name in enumerate(event.sampling.names)
-            if index_name not in dst_index
-        ]
         # fist case - destination index is a subset of source index
-        if set(dst_index).issubset(set(src_index)):
-            # initialize data dictionaries
-            dst_sampling_data = {}
-            dst_event_data = {}
+        if set(dst_index_names).issubset(set(src_index_names)):
+            output_event = _subset_impl(event, src_index_names, dst_index_names)
 
-            # loop over source index levels
-            for src_index_level in event.data.keys():
-                # kept index names
-                dst_index_level = tuple(
-                    (src_index_level[i] for i in dst_index_positions)
-                )
-                # sampling
-                # get source sampling
-                timestamps = event.sampling.data[src_index_level]
-                try:
-                    dst_sampling_data[dst_index_level] = np.append(
-                        dst_sampling_data[dst_index_level], timestamps
-                    )
-                except KeyError:
-                    # create index level
-                    dst_sampling_data[dst_index_level] = np.array(timestamps)
-
-                # event
-                # get source features
-                features = event.data[src_index_level]
-
-                # index names from source index that are going to be converted
-                # to features
-                dst_features = [
-                    feature_name
-                    for feature_name in src_index
-                    if feature_name not in dst_index
-                ]
-                try:
-                    # append collapsed index levels first
-                    for i, j in enumerate(src_index_positions):
-                        dst_event_data[dst_index_level][i].data = np.append(
-                            dst_event_data[dst_index_level][i].data,
-                            [src_index_level[j]] * len(features[0].data),
-                        )
-                    # append other feature data
-                    for i, feature in enumerate(
-                        features, start=len(dst_features)
-                    ):
-                        dst_event_data[dst_index_level][i].data = np.append(
-                            dst_event_data[dst_index_level][i].data,
-                            feature.data,
-                        )
-                except KeyError:
-                    # create index level
-                    dst_event_data[dst_index_level] = [
-                        NumpyFeature(
-                            feature_name,
-                            None,  # sampling=None, removed in pending PRs
-                            np.array(
-                                [src_index_level[i]] * len(features[0].data)
-                            ),
-                        )
-                        for i, feature_name in zip(
-                            src_index_positions, dst_features
-                        )
-                    ] + deepcopy(features)
-
-            # sort data and sampling according to timestamps
-            for dst_index_level in dst_event_data.keys():
-                sorted_idxs = np.argsort(dst_sampling_data[dst_index_level])
-
-                # sampling
-                dst_sampling_data[dst_index_level] = dst_sampling_data[
-                    dst_index_level
-                ][sorted_idxs]
-
-                # event
-                for feature in dst_event_data[dst_index_level]:
-                    feature.data = feature.data[sorted_idxs]
-
-            output_event = NumpyEvent(
-                data=dst_event_data,
-                sampling=NumpySampling(dst_index, dst_sampling_data),
-            )
-
-        elif set(dst_index).issuperset(set(src_index)):
+        elif set(dst_index_names).issuperset(set(src_index_names)):
             pass
 
         return {"event": output_event}
