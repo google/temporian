ea7cd86a2d9c19cd87d555dcd387d261f5d32a61
Diego Marvid <dmarvid@tryolabs.com>
2023-02-17 20:06:57 -0300

    create base version of conversions

--- temporian/implementation/numpy/data/event.py
+++ temporian/implementation/numpy/data/event.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Tuple, Any
 
 import numpy as np
+import pandas as pd
+
 from temporian.core.data.event import Event
 from temporian.core.data.feature import Feature
 from temporian.core.data import dtype
@@ -88,6 +90,92 @@ class NumpyEvent:
             ],
             sampling=self.sampling.names,
         )
+
+    @staticmethod
+    def dataframe_to_event(
+        df: pd.DataFrame,
+        timestamp_index_name: str = "timestamp",
+    ) -> "NumpyEvent":
+        """Function to convert a pandas DataFrame to a NumpyEvent
+
+        Args:
+            df: DataFrame to convert to NumpyEvent
+            timestamp_index_name: Name for timestamp index. Defaults to "timestamp".
+
+        Returns:
+            NumpyEvent: NumpyEvent created from DataFrame
+        """
+
+        # create df without timestamp in index in order to group each index
+        # with its timestamps for conversion
+        df_without_ts_idx = df.reset_index(level="timestamp")
+        index_without_ts = df_without_ts_idx.index
+        sampling = {}
+        data = {}
+
+        for index in index_without_ts.unique():
+            # get the df grouped by timestamp in a specific index
+            index_group = df.groupby(index_without_ts.names).get_group(index)
+
+            # get timestamps of group
+            timestamps = index_group.index.get_level_values(
+                timestamp_index_name
+            )
+            timestamps = timestamps.to_numpy()
+
+            # convert to tuple if not already, useful for single index
+            if type(index) != tuple:
+                index = (index,)
+
+            sampling[index] = timestamps
+
+            # create NumpyFeatures for each column in index_group
+            columns = index_group.loc[index]
+            data[index] = []
+            for column_name in index_group.columns:
+                data[index].append(
+                    NumpyFeature(column_name, columns[column_name].to_numpy())
+                )
+
+        numpy_sampling = NumpySampling(
+            names=index_without_ts.names, data=sampling
+        )
+
+        return NumpyEvent(data=data, sampling=numpy_sampling)
+
+    @staticmethod
+    def event_to_dataframe(event: "NumpyEvent") -> pd.DataFrame:
+        """Function to convert a NumpyEvent to a pandas DataFrame
+
+        Args:
+            event: NumpyEvent to convert to DataFrame
+
+        Returns:
+            pd.DataFrame: DataFrame created from NumpyEvent
+        """
+        df_index = event.sampling.names + ["timestamp"]
+        df_features = event.feature_names
+        columns = df_index + df_features
+
+        df = pd.DataFrame(data=[], columns=columns).set_index(df_index)
+
+        for index, features in event.data.items():
+            timestamps_index = event.sampling.data[index]
+            for i, timestamp in enumerate(timestamps_index):
+                # add timestamp to index
+                new_index = index + (timestamp,)
+                df.loc[new_index, df_features] = [
+                    feature.data[i] for feature in features
+                ]
+
+        # Convert to original dtypes, can be more efficient
+        first_index = list(event.data.keys())[0]
+        first_features = event.data[first_index]
+        df = df.astype(
+            {feature.name: type(feature.data[0]) for feature in first_features}
+        )
+
+        return df
 
     def __repr__(self) -> str:
         return self.data.__repr__() + " " + self.sampling.__repr__()
---
+++ temporian/test/event_conversion_test.py
@@ -0,0 +1,99 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pandas as pd
+import numpy as np
+from absl import logging
+from absl.testing import absltest
+from temporian.implementation.numpy.data.event import NumpyEvent
+from temporian.implementation.numpy.data.event import NumpyFeature
+from temporian.implementation.numpy.data.sampling import NumpySampling
+
+
+class EventConversionTest(absltest.TestCase):
+    def test_df_to_numpy_event(self) -> None:
+        df = pd.DataFrame(
+            [
+                [666964, 1.0, 740.0],
+                [666964, 2.0, 508.0],
+                [574016, 3.0, 573.0],
+            ],
+            columns=["product_id", "timestamp", "costs"],
+        ).set_index(["product_id", "timestamp"])
+
+        numpy_sampling = NumpySampling(
+            data={
+                (666964,): np.array([1.0, 2.0]),
+                (574016,): np.array([3.0]),
+            },
+            names=["product_id"],
+        )
+
+        expected_numpy_event = NumpyEvent(
+            data={
+                (666964,): [
+                    NumpyFeature(data=np.array([740.0, 508.0]), name="costs")
+                ],
+                (574016,): [NumpyFeature(data=np.array([573.0]), name="costs")],
+            },
+            sampling=numpy_sampling,
+        )
+
+        numpy_event = NumpyEvent.dataframe_to_event(df)
+
+        # validate
+        self.assertEqual(
+            True,
+            numpy_event == expected_numpy_event,
+        )
+
+    def test_numpy_event_to_df(self) -> None:
+        numpy_sampling = NumpySampling(
+            data={
+                (666964,): np.array([1.0, 2.0]),
+                (574016,): np.array([3.0]),
+            },
+            names=["product_id"],
+        )
+
+        numpy_event = NumpyEvent(
+            data={
+                (666964,): [
+                    NumpyFeature(data=np.array([740.0, 508.0]), name="costs")
+                ],
+                (574016,): [NumpyFeature(data=np.array([573.0]), name="costs")],
+            },
+            sampling=numpy_sampling,
+        )
+
+        expected_df = pd.DataFrame(
+            [
+                [666964, 1.0, 740.0],
+                [666964, 2.0, 508.0],
+                [574016, 3.0, 573.0],
+            ],
+            columns=["product_id", "timestamp", "costs"],
+        ).set_index(["product_id", "timestamp"])
+
+        df = NumpyEvent.event_to_dataframe(numpy_event)
+
+        # validate
+        self.assertEqual(
+            True,
+            df.equals(expected_df),
+        )
+
+
+if __name__ == "__main__":
+    absltest.main()
