8a24a7bdf2aa801b49be8417d5fc93910a764e1d
Diego Marvid <dmarvid@tryolabs.com>
2023-02-23 20:35:26 -0300

    removed sum & pandas prototype test

--- temporian/implementation/numpy/operators/core_mapping.py
+++ temporian/implementation/numpy/operators/core_mapping.py
@@ -2,14 +2,12 @@ from temporian.implementation.numpy.operators.assign import (
     AssignNumpyImplementation,
 )
 from temporian.implementation.numpy.operators.select import NumpySelectOperator
-from temporian.implementation.numpy.operators.sum import NumpySumOperator
 from temporian.implementation.numpy.operators.arithmetic import (
     ArithmeticNumpyImplementation,
 )
 
 OPERATOR_IMPLEMENTATIONS = {
     "SELECT": NumpySelectOperator,
-    "SUM": NumpySumOperator,
     "ASSIGN": AssignNumpyImplementation,
     "ARITHMETIC": ArithmeticNumpyImplementation,
 }
