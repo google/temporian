59b7168a570d7d80de5eb19f021fba508b1c0bdb
Diego Marvid <dmarvid@tryolabs.com>
2023-03-15 18:01:59 -0300

    create df in every test

--- temporian/implementation/numpy/operators/tests/select_test.py
+++ temporian/implementation/numpy/operators/tests/select_test.py
@@ -25,18 +25,18 @@ class SelectOperatorTest(absltest.TestCase):
     """Select operator test."""
 
     def setUp(self):
-        A = 0
-        B = 1
-        C = 2
+        self.A = 0
+        self.B = 1
+        self.C = 2
 
         self.df = pd.DataFrame(
             [
-                [A, 1, 10.0, -1.0, 0.0],
-                [A, 2, np.nan, -2.0, 32.0],
-                [B, 3, 12.0, -3.0, 27.0],
-                [B, 4, 13.0, -4.0, 28.0],
-                [C, 5, 14.0, np.nan, 29.0],
-                [C, 6, 15.0, -6.0, np.nan],
+                [self.A, 1, 10.0, -1.0, 0.0],
+                [self.A, 2, np.nan, -2.0, 32.0],
+                [self.B, 3, 12.0, -3.0, 27.0],
+                [self.B, 4, 13.0, -4.0, 28.0],
+                [self.C, 5, 14.0, np.nan, 29.0],
+                [self.C, 6, 15.0, -6.0, np.nan],
             ],
             columns=["store_id", "timestamp", "sales", "costs", "weather"],
         )
@@ -49,51 +49,49 @@ class SelectOperatorTest(absltest.TestCase):
 
     def test_select_one_feature(self) -> None:
         """Test correct select operator for one feature selection."""
-
-        selected_feature = "sales"
-
-        # drop all columns in features except selected_feature
-        new_df = self.df.copy()
-        new_df = new_df.drop(
-            columns=[col for col in self.features if col != selected_feature]
+        new_df = pd.DataFrame(
+            [
+                [self.A, 1, 10.0],
+                [self.A, 2, np.nan],
+                [self.B, 3, 12.0],
+                [self.B, 4, 13.0],
+                [self.C, 5, 14.0],
+                [self.C, 6, 15.0],
+            ],
+            columns=["store_id", "timestamp", "sales"],
         )
 
-        operator = select.NumpySelectOperator(feature_names=selected_feature)
+        operator = select.NumpySelectOperator(feature_names="sales")
         selected_event = operator(self.numpy_event)["event"]
 
         expected_event = NumpyEvent.from_dataframe(
             new_df, index_names=["store_id"]
         )
 
-        self.assertEqual(
-            True,
-            selected_event == expected_event,
-        )
+        self.assertTrue(selected_event == expected_event)
 
     def test_select_multiple_features(self) -> None:
         """Test correct select operator for multiple features selection."""
-
-        selected_features = ["sales", "costs"]
-
-        # drop all columns in features except selected_feature
-        new_df = self.df.copy()
-        new_df = new_df.drop(
-            columns=[
-                col for col in self.features if col not in selected_features
-            ]
+        new_df = pd.DataFrame(
+            [
+                [self.A, 1, 10.0, -1.0],
+                [self.A, 2, np.nan, -2.0],
+                [self.B, 3, 12.0, -3.0],
+                [self.B, 4, 13.0, -4.0],
+                [self.C, 5, 14.0, np.nan],
+                [self.C, 6, 15.0, -6.0],
+            ],
+            columns=["store_id", "timestamp", "sales", "costs"],
         )
 
-        operator = select.NumpySelectOperator(feature_names=selected_features)
+        operator = select.NumpySelectOperator(feature_names=["sales", "costs"])
         selected_event = operator(self.numpy_event)["event"]
 
         expected_event = NumpyEvent.from_dataframe(
             new_df, index_names=["store_id"]
         )
 
-        self.assertEqual(
-            True,
-            selected_event == expected_event,
-        )
+        self.assertTrue(selected_event == expected_event)
 
 
 if __name__ == "__main__":
