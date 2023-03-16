f8e390417a9ca2a60c929f3759947a6528b2cda4
Diego Marvid <dmarvid@tryolabs.com>
2023-03-16 16:02:11 -0300

    remove exceptions, leave it to pandas

--- temporian/io/read_event.py
+++ temporian/io/read_event.py
@@ -14,26 +14,17 @@ def read_event(
 
     Args:
         path (str): Path to the file.
-        timestamp_column (str): Name of the column to be used as timestamp for the event.
-        index_names (List[str], optional): Names of the DataFrame columns to be used as index for the event. Defaults to [].
+        timestamp_column (str): Name of the column to be used as timestamp for
+            the event.
+        index_names (List[str], optional): Names of the DataFrame columns to be
+            used as index for the event. Defaults to [].
         sep (str, optional): Separator to use. Defaults to ",".
 
 
     Returns:
         NumpyEvent: NumpyEvent read from file.
 
-    Raises:
-        FileNotFoundError: If file does not exist.
-        ValueError: If file is not a csv file.
-
     """
-    # check file exists and is a csv
-    path = Path(path)
-    if not path.exists():
-        raise FileNotFoundError(f"File {path} does not exist.")
-    if path.suffix != ".csv":
-        raise ValueError(f"File {path} is not a csv file.")
-
     df = pd.read_csv(path, sep=sep)
     return NumpyEvent.from_dataframe(
         df, index_names=index_names, timestamp_column=timestamp_column
