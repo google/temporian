06ecdf5394fa363fbb9ca1a4bed54065ed814414
Diego Marvid <dmarvid@tryolabs.com>
2023-03-17 13:58:45 -0300

    fix schema and add it to prototype test

--- temporian/test/prototype_test_numpy.py
+++ temporian/test/prototype_test_numpy.py
@@ -11,11 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from absl import logging
-from absl.testing import absltest
-
 import numpy as np
 import pandas as pd
+from absl import logging
+from absl.testing import absltest
 
 from temporian.core import evaluator
 from temporian.core.data.event import Event, Feature
@@ -35,7 +34,7 @@ class PrototypeTest(absltest.TestCase):
         BOOK_ID = 2
         PIXEL_ID = 3
 
-        self.event_1 = NumpyEvent.from_dataframe(
+        self.event_1_data = NumpyEvent.from_dataframe(
             pd.DataFrame(
                 data=[
                     [TRYOLABS_SHOP, MATE_ID, 0.0, 14],
@@ -52,7 +51,7 @@ class PrototypeTest(absltest.TestCase):
             index_names=["store_id", "product_id"],
         )
 
-        self.event_2 = NumpyEvent.from_dataframe(
+        self.event_2_data = NumpyEvent.from_dataframe(
             pd.DataFrame(
                 data=[
                     [TRYOLABS_SHOP, MATE_ID, 0.0, -14],
@@ -95,18 +94,8 @@ class PrototypeTest(absltest.TestCase):
         )
 
     def test_prototype(self) -> None:
-        sampling = Sampling(["store_id", "product_id"])
-
-        event_1 = Event(
-            [Feature("sales", int)],
-            sampling=sampling,
-            creator=None,
-        )
-
-        event_2 = Event(
-            [Feature("costs", int)],
-            sampling=sampling,
-        )
+        event_1 = self.event_1_data.schema()
+        event_2 = self.event_2_data.schema()
 
         # assign second event to first
         output_event = assign(event_1, event_2)
@@ -125,22 +114,15 @@ class PrototypeTest(absltest.TestCase):
             output_event,
             input_data={
                 # left event specified from disk
-                event_1: self.event_1,
-                event_2: self.event_2,
+                event_1: self.event_1_data,
+                event_2: self.event_2_data,
             },
             backend="numpy",
         )
 
-        if self.expected_output_event != output_event_numpy:
-            print("expected")
-            print(self.expected_output_event.to_dataframe())
-            print("actual")
-            print(output_event_numpy.to_dataframe())
-
         # validate
         self.assertEqual(
-            True,
-            self.expected_output_event == output_event_numpy,
+            self.expected_output_event, output_event_numpy[output_event]
         )
 
 
