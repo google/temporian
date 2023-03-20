7a8ae00a0e42545dca433d09cb8c5111ae9ece97
Diego Marvid <dmarvid@tryolabs.com>
2023-03-20 20:50:10 -0300

    change select for new op implementation

--- temporian/implementation/numpy/operators/core_mapping.py
+++ temporian/implementation/numpy/operators/core_mapping.py
@@ -5,19 +5,21 @@ from temporian.implementation.numpy.operators.arithmetic import (
     ArithmeticNumpyImplementation,
 )
 from temporian.implementation.numpy.operators.lag import LagNumpyImplementation
-from temporian.implementation.numpy.operators.select import NumpySelectOperator
 from temporian.implementation.numpy.operators.simple_moving_average import (
     SimpleMovingAverageNumpyImplementation as SmaImp,
 )
 from temporian.core.operators.simple_moving_average import (
     SimpleMovingAverage as SmaDef,
 )
+from temporian.implementation.numpy.operators.select import (
+    SelectNumpyImplementation,
+)
 
 # TODO: Use a registration mechanism instead.
 # TODO: Make sure the implementation key is defined only one (in the operator
 # definition).
 OPERATOR_IMPLEMENTATIONS = {
-    "SELECT": NumpySelectOperator,
+    "SELECT": SelectNumpyImplementation,
     "ASSIGN": AssignNumpyImplementation,
     "ARITHMETIC": ArithmeticNumpyImplementation,
     "LAG": LagNumpyImplementation,
