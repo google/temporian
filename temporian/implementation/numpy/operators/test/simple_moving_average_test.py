4b58ec0dadeb3bfc2378ff7f42d0a0bbdda1c5a9
Diego Marvid <dmarvid@tryolabs.com>
2023-03-24 10:57:28 -0300

    implement events with dataframe

--- temporian/implementation/numpy/operators/test/simple_moving_average_test.py
+++ temporian/implementation/numpy/operators/test/simple_moving_average_test.py
@@ -15,6 +15,7 @@
 from absl.testing import absltest
 import math
 
+import pandas as pd
 import numpy as np
 
 from temporian.core.operators.simple_moving_average import SimpleMovingAverage
@@ -38,143 +39,113 @@ class SimpleMovingAverageOperatorTest(absltest.TestCase):
     def test_flat(self):
         """A simple time sequence."""
 
-        op = SimpleMovingAverage(
-            event=event_lib.input_event(
+        input_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
                 [
-                    feature_lib.Feature(name="a", dtype=dtype_lib.FLOAT64),
-                    feature_lib.Feature(name="b", dtype=dtype_lib.FLOAT64),
-                ]
-            ),
+                    [10.0, 20.0, 1],
+                    [11.0, 21.0, 2],
+                    [12.0, 22.0, 3],
+                    [13.0, 23.0, 5],
+                    [14.0, 24.0, 20],
+                ],
+                columns=["a", "b", "timestamp"],
+            )
+        )
+
+        op = SimpleMovingAverage(
+            event=input_data.schema(),
             window_length=5,
             sampling=None,
         )
         self.assertEqual(op.list_matching_io_samplings(), [("event", "event")])
         instance = SimpleMovingAverageNumpyImplementation(op)
 
-        input_data = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([10.0, 11.0, 12.0, 13.0, 14.0]),
-                    ),
-                    NumpyFeature(
-                        name="b",
-                        data=np.array([20, 21, 22, 23, 24]),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={(): np.array([1, 2, 3, 5, 20], dtype=np.float64)},
-            ),
-        )
         output = instance(event=input_data)
-        expected_output = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array([10.0, 10.5, 11.0, 11.5, 14.0]),
-                    ),
-                    NumpyFeature(
-                        name="sma_b",
-                        data=np.array([20.0, 20.5, 21.0, 21.5, 24.0]),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={(): np.array([1, 2, 3, 5, 20], dtype=np.float64)},
-            ),
+
+        expected_output = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [10.0, 20.0, 1],
+                    [10.5, 20.5, 2],
+                    [11.0, 21.0, 3],
+                    [11.5, 21.5, 5],
+                    [14.0, 24.0, 20],
+                ],
+                columns=["sma_a", "sma_b", "timestamp"],
+            )
         )
+
         self.assertEqual(repr(output), repr({"event": expected_output}))
 
     def test_with_index(self):
         """Indexed time sequences."""
 
-        op = SimpleMovingAverage(
-            event=event_lib.input_event(
-                [feature_lib.Feature(name="a", dtype=dtype_lib.FLOAT64)],
-                index=["x", "y"],
+        input_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    ["X1", "Y1", 10.0, 1],
+                    ["X1", "Y1", 11.0, 2],
+                    ["X1", "Y1", 12.0, 3],
+                    ["X2", "Y1", 13.0, 1.1],
+                    ["X2", "Y1", 14.0, 2.1],
+                    ["X2", "Y1", 15.0, 3.1],
+                    ["X2", "Y2", 16.0, 1.2],
+                    ["X2", "Y2", 17.0, 2.2],
+                    ["X2", "Y2", 18.0, 3.2],
+                ],
+                columns=["x", "y", "a", "timestamp"],
             ),
+            index_names=["x", "y"],
+        )
+
+        op = SimpleMovingAverage(
+            event=input_data.schema(),
             window_length=5,
             sampling=None,
         )
         self.assertEqual(op.list_matching_io_samplings(), [("event", "event")])
         instance = SimpleMovingAverageNumpyImplementation(op)
 
-        input_data = NumpyEvent(
-            data={
-                ("X1", "Y1"): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([10.0, 11.0, 12.0]),
-                    )
-                ],
-                ("X2", "Y1"): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([13.0, 14.0, 15.0]),
-                    )
-                ],
-                ("X2", "Y2"): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([16.0, 17.0, 18.0]),
-                    )
-                ],
-            },
-            sampling=NumpySampling(
-                index=["x", "y"],
-                data={
-                    ("X1", "Y1"): np.array([1, 2, 3], dtype=np.float64),
-                    ("X2", "Y1"): np.array([1.1, 2.1, 3.1], dtype=np.float64),
-                    ("X2", "Y2"): np.array([1.2, 2.2, 3.2], dtype=np.float64),
-                },
-            ),
-        )
         output = instance(event=input_data)
 
-        expected_output = NumpyEvent(
-            data={
-                ("X1", "Y1"): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array([10.0, 10.5, 11.0]),
-                    )
+        expected_output = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    ["X1", "Y1", 10.0, 1],
+                    ["X1", "Y1", 10.5, 2],
+                    ["X1", "Y1", 11.0, 3],
+                    ["X2", "Y1", 13.0, 1.1],
+                    ["X2", "Y1", 13.5, 2.1],
+                    ["X2", "Y1", 14.0, 3.1],
+                    ["X2", "Y2", 16.0, 1.2],
+                    ["X2", "Y2", 16.5, 2.2],
+                    ["X2", "Y2", 17.0, 3.2],
                 ],
-                ("X2", "Y1"): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array([13.0, 13.5, 14.0]),
-                    )
-                ],
-                ("X2", "Y2"): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array([16.0, 16.5, 17.0]),
-                    )
-                ],
-            },
-            sampling=NumpySampling(
-                index=["x", "y"],
-                data={
-                    ("X1", "Y1"): np.array([1.0, 2.0, 3.0], dtype=np.float64),
-                    ("X2", "Y1"): np.array([1.1, 2.1, 3.1], dtype=np.float64),
-                    ("X2", "Y2"): np.array([1.2, 2.2, 3.2], dtype=np.float64),
-                },
+                columns=["x", "y", "sma_a", "timestamp"],
             ),
+            index_names=["x", "y"],
         )
-        self.assertEqual(repr(output), repr({"event": expected_output}))
+
+        self.assertEqual(output["event"], expected_output)
 
     def test_with_sampling(self):
         """Time sequenes with user provided sampling."""
 
+        input_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [10.0, 1],
+                    [11.0, 2],
+                    [12.0, 3],
+                    [13.0, 5],
+                    [14.0, 6],
+                ],
+                columns=["a", "timestamp"],
+            )
+        )
+
         op = SimpleMovingAverage(
-            event=event_lib.input_event(
-                [feature_lib.Feature(name="a", dtype=dtype_lib.FLOAT64)]
-            ),
+            event=input_data.schema(),
             window_length=3,
             sampling=event_lib.input_event([]),
         )
@@ -183,124 +154,98 @@ class SimpleMovingAverageOperatorTest(absltest.TestCase):
         )
         instance = SimpleMovingAverageNumpyImplementation(op)
 
-        input_data = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([10.0, 11.0, 12.0, 13.0, 14.0]),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={(): np.array([1, 2, 3, 5, 6], dtype=np.float64)},
-            ),
-        )
-
-        sampling_data = NumpyEvent(
-            data={(): []},
-            sampling=NumpySampling(
-                index=[],
-                data={
-                    (): np.array(
-                        [-1.0, 1.0, 1.1, 3, 3.5, 6, 10], dtype=np.float64
-                    )
-                },
-            ),
+        sampling_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [-1.0],
+                    [1.0],
+                    [1.1],
+                    [3.0],
+                    [3.5],
+                    [6.0],
+                    [10.0],
+                ],
+                columns=["timestamp"],
+            )
         )
 
         output = instance(event=input_data, sampling=sampling_data)
 
-        expected_output = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array(
-                            [math.nan, 10.0, 10.0, 11.0, 11.0, 13.0, math.nan]
-                        ),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={
-                    (): np.array(
-                        [-1.0, 1.0, 1.1, 3.0, 3.5, 6.0, 10.0], dtype=np.float64
-                    )
-                },
-            ),
+        expected_output = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [math.nan, -1.0],
+                    [10.0, 1.0],
+                    [10.0, 1.1],
+                    [11.0, 3.0],
+                    [11.0, 3.5],
+                    [13.0, 6.0],
+                    [math.nan, 10.0],
+                ],
+                columns=["sma_a", "timestamp"],
+            )
         )
-        self.assertEqual(repr(output), repr({"event": expected_output}))
+
+        self.assertEqual(output["event"], expected_output)
 
     def test_with_nan(self):
         """The input features contains nan values."""
 
+        input_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [math.nan, 1],
+                    [11.0, 2],
+                    [math.nan, 3],
+                    [13.0, 5],
+                    [14.0, 6],
+                ],
+                columns=["a", "timestamp"],
+            )
+        )
+
         op = SimpleMovingAverage(
-            event=event_lib.input_event(
-                [feature_lib.Feature(name="a", dtype=dtype_lib.FLOAT64)]
-            ),
+            event=input_data.schema(),
             window_length=1,
             sampling=event_lib.input_event([]),
         )
         instance = SimpleMovingAverageNumpyImplementation(op)
 
-        input_data = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="a",
-                        data=np.array([math.nan, 11.0, math.nan, 13.0, 14.0]),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={(): np.array([1, 2, 3, 5, 6], dtype=np.float64)},
-            ),
-        )
-
-        sampling_data = NumpyEvent(
-            data={(): []},
-            sampling=NumpySampling(
-                index=[],
-                data={
-                    (): np.array([1, 2, 2.5, 3, 3.5, 4, 5, 6], dtype=np.float64)
-                },
-            ),
+        sampling_data = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [1],
+                    [2],
+                    [2.5],
+                    [3],
+                    [3.5],
+                    [4],
+                    [5],
+                    [6],
+                ],
+                columns=["timestamp"],
+            )
         )
 
         output = instance(event=input_data, sampling=sampling_data)
 
-        expected_output = NumpyEvent(
-            data={
-                (): [
-                    NumpyFeature(
-                        name="sma_a",
-                        data=np.array(
-                            [
-                                math.nan,
-                                11.0,
-                                11,
-                                11.0,
-                                math.nan,
-                                math.nan,
-                                13.0,
-                                13.5,
-                            ]
-                        ),
-                    ),
-                ]
-            },
-            sampling=NumpySampling(
-                index=[],
-                data={
-                    (): np.array([1, 2, 2.5, 3, 3.5, 4, 5, 6], dtype=np.float64)
-                },
-            ),
+        expected_output = NumpyEvent.from_dataframe(
+            pd.DataFrame(
+                [
+                    [math.nan, 1],
+                    [11.0, 2],
+                    [11.0, 2.5],
+                    [11.0, 3],
+                    [math.nan, 3.5],
+                    [math.nan, 4],
+                    [13.0, 5],
+                    [13.5, 6],
+                ],
+                columns=["sma_a", "timestamp"],
+            )
         )
-        self.assertEqual(repr(output), repr({"event": expected_output}))
+
+        self.assertEqual(output["event"], expected_output)
 
 
 if __name__ == "__main__":
