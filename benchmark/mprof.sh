1f6f893c95904fda99bfa6b9856f660e7324221b
Ian Spektor <ispektor@tryolabs.com>
2023-03-31 18:54:20 +0200

    add TODO

--- benchmark/mprof.sh
+++ benchmark/mprof.sh
@@ -1,7 +1,11 @@
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
+# TODO: provide flag to `bazel run benchmark:memory` to choose between
+# line-by-line memory consumption and memory over time plot
+
+# display line-by-line memory consumption
 python -m memory_profiler benchmark/basic.py
 
-# display memory over time plot instead of line-by-line consumption:
+# display memory over time plot
 # mprof run --python python benchmark/basic.py
 # mprof plot --flame
