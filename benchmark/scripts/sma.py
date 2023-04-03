9e1797dc0ea58d0ebb4c4c395d92bbbd609bf8dc
Ian Spektor <ispektor@tryolabs.com>
2023-04-03 12:22:29 +0200

    fix assign -> glue rename after merge

--- benchmark/scripts/sma.py
+++ benchmark/scripts/sma.py
@@ -62,7 +62,7 @@ def main():
 
     event = event_data.schema()
 
-    sma = tp.sma(event, window_length=10)
+    sma = tp.simple_moving_average(event, window_length=10)
 
     res: NumpyEvent = tp.evaluate(
         sma,
