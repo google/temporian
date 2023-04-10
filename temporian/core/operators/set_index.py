68c2794c63c6c3b9fb40399129ece031deed0c69
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-04-10 11:29:27 -0300

    Add set_index function docstring

--- temporian/core/operators/set_index.py
+++ temporian/core/operators/set_index.py
@@ -115,4 +115,51 @@ operator_lib.register_operator(SetIndexOperator)
 def set_index(
     event: Event, feature_names: List[str], append: bool = False
 ) -> Event:
+    """
+    Sets one or more index columns for the given `Event` object and returns
+    a new `Event` object with the updated index. Optionally, it can also append
+    the new index columns to the existing index.
+
+    Args:
+        event:
+            The input `Event` object for which the index is to be set or
+            updated.
+        feature_names:
+            A list of feature names (strings) that should be used as the new
+            index. These feature names should already exist in the input
+            `Event`.
+        append:
+            A flag indicating whether the new index should be appended to the
+            existing index (True) or replace it (False). Defaults to `False`.
+
+    Returns:
+        A new `Event` object with the updated index, where the specified feature
+        names have been set or appended as index columns, based on the `append`
+        flag.
+
+    Raises:
+        KeyError:
+            If any of the specified feature_names are not found in the input
+            `Event`.
+
+    Examples:
+        Given an input `Event` with index names ['A', 'B', 'C'] and features
+        names ['X', 'Y', 'Z']:
+
+        1. set_index(event, feature_names=['X'], append=False)
+           Output `Event` will have index names ['X'] and features names
+           ['Y', 'Z'].
+
+        2. set_index(event, feature_names=['X', 'Y'], append=False)
+           Output `Event` will have index names ['X', 'Y'] and
+           features names ['Z'].
+
+        3. set_index(event, feature_names=['X', 'Y'], append=True)
+           Output `Event` will have index names ['A', 'B', 'C', 'X', 'Y'] and
+           features names ['Z'].
+
+    Notes:
+        The input `Event` object remains unchanged. The function returns a new
+        `Event` object with the specified index changes.
+    """
     return SetIndexOperator(event, feature_names, append).outputs["event"]
