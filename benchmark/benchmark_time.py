c9993b6d448187374f76f645ae818340df2e2589
Braulio Ríos <braulio.rios@tryolabs.com>
2023-04-19 11:06:46 -0300

    Added benchmark for cast operator

--- benchmark/benchmark_time.py
+++ benchmark/benchmark_time.py
@@ -146,6 +146,28 @@ def benchmark_propagate(runner):
         )
 
 
+def benchmark_cast(runner):
+    runner.add_separator()
+    for n in [100, 1_000_000]:
+        for check in [False, True]:
+            ds = _build_toy_dataset(n)
+
+            event = ds.schema()
+            output = tp.cast(
+                event,
+                {
+                    "data_1": tp.dtype.DType.INT32,
+                    "data_2": tp.dtype.DType.FLOAT32,
+                },
+                check_overflow=check,
+            )
+
+            runner.benchmark(
+                f"cast({check=}):{n}",
+                lambda: tp.evaluate(output, input_data={event: ds}),
+            )
+
+
 def benchmark_from_dataframe(runner):
     runner.add_separator()
     # TODO: Add num_timestamps = 100_000 and 1_000_000 when from_dataframe is
@@ -198,6 +220,7 @@ def main():
     benchmark_calendar_day_of_month(runner)
     benchmark_sample(runner)
     benchmark_propagate(runner)
+    benchmark_cast(runner)
 
     print("All results (again)")
     runner.print_results()
