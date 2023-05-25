242c4b317517494a3359d846a2235b1de8fdb8d9
Ian Spektor <ispektor@tryolabs.com>
2023-05-25 15:46:56 +0200

    Doc improvements (#137)
    * rename Code Reference to API Reference
    
    * remove SUMMARY file from search
    
    * move bazel load to top of file
    
    * remove unused skipped_paths
    
    * fix formatting
    
    * Revert "move bazel load to top of file"
    
    This reverts commit 64701fa192483c664cc8fcd5c9436678d2a01318.
    
    * move bazel load to top of file

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -12,7 +12,6 @@ nav = mkdocs_gen_files.Nav()
 
 SRC_PATH = Path("temporian")
 
-skipped_paths = []
 
 for path in sorted(SRC_PATH.rglob("*.py")):
     if path.parent.name not in ["test", "tests"]:
@@ -36,8 +35,11 @@ for path in sorted(SRC_PATH.rglob("*.py")):
             print("::: " + identifier, file=fd)
 
         mkdocs_gen_files.set_edit_path(full_doc_path, path)
-    else:
-        skipped_paths.append(str(path))
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    # Remove SUMMARY file from search
+    nav_file.writelines("---")
+    nav_file.writelines("search:")
+    nav_file.writelines("  exclude: true")
+    nav_file.writelines("---")
     nav_file.writelines(nav.build_literate_nav())
