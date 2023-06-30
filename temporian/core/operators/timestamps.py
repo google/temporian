10e3eb394c12b279a34d2b61684ba31e118bf1a1
Braulio Ríos <braulio.rios@tryolabs.com>
2023-06-30 15:56:18 -0300

    Structure for timestamps & enumerate ops

---
+++ temporian/core/operators/timestamps.py
@@ -0,0 +1,77 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Timestamps operator class and public API function definitions."""
+
+from temporian.core import operator_lib
+from temporian.core.data.node import Node, create_node_new_features_new_sampling
+from temporian.core.operators.base import Operator
+from temporian.proto import core_pb2 as pb
+
+
+class Timestamps(Operator):
+    def __init__(self, input: Node, param: float):
+        super().__init__()
+
+        self.add_input("input", input)
+        self.add_attribute("param", param)
+
+        self.add_output(
+            "output",
+            create_node_new_features_new_sampling(
+                features=[],
+                indexes=input.schema.indexes,
+                is_unix_timestamp=input.schema.is_unix_timestamp,
+                creator=self,
+            ),
+        )
+
+        self.check()
+
+    @classmethod
+    def build_op_definition(cls) -> pb.OperatorDef:
+        return pb.OperatorDef(
+            key="TIMESTAMPS",
+            attributes=[
+                pb.OperatorDef.Attribute(
+                    key="param",
+                    type=pb.OperatorDef.Attribute.Type.FLOAT_64,
+                    is_optional=False,
+                ),
+            ],
+            inputs=[pb.OperatorDef.Input(key="input")],
+            outputs=[pb.OperatorDef.Output(key="output")],
+        )
+
+
+operator_lib.register_operator(Timestamps)
+
+
+def timestamps(input: Node, param: float) -> Node:
+    """<Text>
+
+    Args:
+        input: <Text>
+        param: <Text>
+
+    Example:
+        <Text>
+
+    Returns:
+        <Text>
+    """
+
+    return Timestamps(input=input, param=param).outputs["output"]
+
---
+++ temporian/implementation/numpy/operators/timestamps.py
@@ -0,0 +1,55 @@
+# Copyright 2021 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Implementation for the Timestamps operator."""
+
+
+from typing import Dict
+import numpy as np
+
+from temporian.implementation.numpy.data.event_set import IndexData, EventSet
+from temporian.core.operators.timestamps import Timestamps
+from temporian.implementation.numpy import implementation_lib
+from temporian.implementation.numpy.operators.base import OperatorImplementation
+
+class TimestampsNumpyImplementation(OperatorImplementation):
+
+    def __init__(self, operator: Timestamps) -> None:
+        assert isinstance(operator, Timestamps)
+        super().__init__(operator)
+
+    def __call__(
+        self, input: EventSet) -> Dict[str, EventSet]:
+        assert isinstance(self.operator, Timestamps)
+
+        output_schema = self.output_schema("output")
+
+        # Create output EventSet
+        output_evset = EventSet(data={}, schema=output_schema)
+
+        # Fill output EventSet's data
+        for index_key, index_data in input.data.items():
+            output_evset[index_key] = IndexData(
+                [],
+                np.array([1], dtype=np.float64),
+                schema=output_schema,
+            )
+
+        return {"output": output_evset}
+
+
+implementation_lib.register_operator_implementation(
+    Timestamps, TimestampsNumpyImplementation
+)
