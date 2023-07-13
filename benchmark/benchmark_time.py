5acfe0282fcb77297cb9e18ee95647e0d45a3c42
Mathieu Guillame-Bert <achoum@gmail.com>
2023-07-13 14:43:02 +0200

    Update benchmark_time.py

--- benchmark/benchmark_time.py
+++ benchmark/benchmark_time.py
@@ -265,7 +265,7 @@ def benchmark_add_index(runner):
                     "feature_6": feature_6,
                 }
             ),
-            # indexes=["index_1", "index_2"], # DO NOT SUBMIT
+            indexes=["index_1", "index_2"],
         )
 
         node = evset.node()
