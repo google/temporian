97cf9d23b00033348c862119582291b5635fde8f
Braulio Ríos <braulio.rios@tryolabs.com>
2023-08-22 19:56:24 -0300

    Unit tests for since_last with steps

--- temporian/implementation/numpy/operators/test/since_last_test.py
+++ temporian/implementation/numpy/operators/test/since_last_test.py
@@ -62,6 +62,39 @@ class SinceLastOperatorTest(absltest.TestCase):
 
         self.assertEqual(output, expected_output)
 
+    def test_no_sampling_2steps(self):
+        steps = 2
+        event_data = from_pandas(
+            pd.DataFrame(
+                {
+                    "timestamp": [1, 5, 8, 9, 1, 1, 2, 2, 2],
+                    "x": [1, 1, 1, 1, 2, 2, 2, 2, 2],
+                }
+            ),
+            indexes=["x"],
+        )
+        event = event_data.node()
+
+        expected_output = from_pandas(
+            pd.DataFrame(
+                {
+                    "timestamp": [1, 5, 8, 9, 1, 1, 2, 2, 2],
+                    "x": [1, 1, 1, 1, 2, 2, 2, 2, 2],
+                    "since_last": [nan, nan, 7, 4, nan, nan, 1, 1, 0],
+                }
+            ),
+            indexes=["x"],
+        )
+
+        # Run op
+        op = SinceLast(input=event, steps=steps)
+        op.outputs["output"].check_same_sampling(event)
+
+        instance = SinceLastNumpyImplementation(op)
+        output = instance.call(input=event_data)["output"]
+
+        self.assertEqual(output, expected_output)
+
     def test_with_sampling(self):
         event_data = from_pandas(
             pd.DataFrame({"timestamp": [1, 2, 2, 4]}),
@@ -84,6 +117,38 @@ class SinceLastOperatorTest(absltest.TestCase):
 
         # Run op
         op = SinceLast(input=event, sampling=sampling)
+        op.outputs["output"].check_same_sampling(sampling)
+
+        instance = SinceLastNumpyImplementation(op)
+        output = instance.call(input=event_data, sampling=sampling_data)[
+            "output"
+        ]
+
+        self.assertEqual(output, expected_output)
+
+    def test_with_sampling_2steps(self):
+        steps = 2
+        event_data = from_pandas(
+            pd.DataFrame({"timestamp": [1, 2, 2, 4]}),
+        )
+        event = event_data.node()
+
+        sampling_data = from_pandas(
+            pd.DataFrame({"timestamp": [-1, 1, 1.5, 2, 2, 2.1, 4, 5]})
+        )
+        sampling = sampling_data.node()
+
+        expected_output = from_pandas(
+            pd.DataFrame(
+                {
+                    "timestamp": [-1, 1, 1.5, 2, 2, 2.1, 4, 5],
+                    "since_last": [nan, nan, nan, 0, 0, 0.1, 2, 3],
+                }
+            )
+        )
+
+        # Run op
+        op = SinceLast(input=event, steps=steps, sampling=sampling)
         op.outputs["output"].check_same_sampling(sampling)
 
         instance = SinceLastNumpyImplementation(op)
