5f88dfef1a42da493311d2c689c032080859d97c
Braulio Ríos <braulio.rios@tryolabs.com>
2023-09-07 21:06:33 -0300

    Added tests for weekdays and end of year (tick calendar)

--- temporian/implementation/numpy/operators/test/tick_calendar_test.py
+++ temporian/implementation/numpy/operators/test/tick_calendar_test.py
@@ -13,11 +13,9 @@
 # limitations under the License.
 
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from absl.testing import absltest
 
-import numpy as np
-from temporian.core.data.duration_utils import convert_timestamps_to_datetimes
 from temporian.core.operators.tick_calendar import TickCalendar
 from temporian.implementation.numpy.data.io import event_set
 from temporian.implementation.numpy.operators.tick_calendar import (
@@ -33,7 +31,9 @@ class TickCalendarOperatorTest(absltest.TestCase):
     def setUp(self):
         pass
 
-    def test_base(self):
+    def test_end_of_month_seconds(self):
+        # All seconds at mday=31, should only be valid for months 1, 3, 5
+
         evset = event_set(
             timestamps=[
                 datetime(2020, 1, 1, 0, 0, 0),
@@ -76,12 +76,99 @@ class TickCalendarOperatorTest(absltest.TestCase):
         testOperatorAndImp(self, op, instance)
         output = instance.call(input=evset)["output"]
 
-        print("Result:")
-        print(
-            convert_timestamps_to_datetimes(
-                output.get_arbitrary_index_data().timestamps
-            )
+        assertEqualEventSet(self, output, expected_output)
+
+    def test_end_of_year_minutes(self):
+        # All hours/minutes from 30/12/2019 to 2/1/2020
+
+        evset = event_set(
+            timestamps=[
+                # 4 days: 2 on 2019 + 2 on 2020
+                datetime(2019, 12, 30, 0, 0, 0),
+                datetime(2020, 1, 2, 23, 59, 59),  # 2/1 at 23:59:59
+            ],
         )
+        node = evset.node()
+
+        # Expected timestamps: all hours/minutes in 4 days
+        timestamps = []
+        for day, month, year in [
+            (30, 12, 2019),
+            (31, 12, 2019),
+            (1, 1, 2020),
+            (2, 1, 2020),
+        ]:
+            for hour in range(24):
+                for minute in range(60):
+                    timestamps += [datetime(year, month, day, hour, minute, 0)]
+        expected_output = event_set(
+            timestamps=timestamps,
+        )
+
+        # Run op
+        op = TickCalendar(
+            input=node,
+            min_second=0,
+            max_second=0,
+            min_minute=0,
+            max_minute=59,
+            min_hour=0,
+            max_hour=23,
+            min_day_of_month=1,  # any day
+            max_day_of_month=31,
+            min_month=1,  # any month
+            max_month=12,
+            min_day_of_week=0,
+            max_day_of_week=6,
+        )
+        instance = TickCalendarNumpyImplementation(op)
+        testOperatorAndImp(self, op, instance)
+        output = instance.call(input=evset)["output"]
+
+        assertEqualEventSet(self, output, expected_output)
+
+    def test_weekdays(self):
+        # All exact hours from all saturdays in 2023
+
+        evset = event_set(
+            timestamps=[
+                datetime(2023, 1, 1),
+                datetime(2023, 12, 31, 23, 0, 0),
+            ],
+        )
+        node = evset.node()
+
+        # Expected timestamps: all hours/minutes in 4 days
+        timestamps = []
+        day = datetime(2023, 1, 7)  # First saturday
+        one_week = timedelta(days=7)
+        while day.year < 2024:
+            for hour in range(24):
+                timestamps += [day + timedelta(hours=hour)]
+            day += one_week
+        expected_output = event_set(
+            timestamps=timestamps,
+        )
+
+        # Run op
+        op = TickCalendar(
+            input=node,
+            min_second=0,
+            max_second=0,
+            min_minute=0,
+            max_minute=0,
+            min_hour=0,
+            max_hour=23,  # all hours
+            min_day_of_month=1,  # any month day
+            max_day_of_month=31,
+            min_month=1,  # any month
+            max_month=12,
+            min_day_of_week=6,  # saturday
+            max_day_of_week=6,  # saturday
+        )
+        instance = TickCalendarNumpyImplementation(op)
+        testOperatorAndImp(self, op, instance)
+        output = instance.call(input=evset)["output"]
 
         assertEqualEventSet(self, output, expected_output)
 
