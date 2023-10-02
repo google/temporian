31c3a596c714a61a06c55164fa539f1915bb01d6
Ian Spektor <ispektor@tryolabs.com>
2023-10-02 16:50:45 +0200

    reduce logic in tests

--- temporian/core/operators/window/test/test_simple_moving_average.py
+++ temporian/core/operators/window/test/test_simple_moving_average.py
@@ -38,160 +38,83 @@ class SimpleMovingAverageTest(TestCase):
             "window": 5.0,
             "output_feature": _f64([]),
         },
-        {  # empty f32 w sampling
+        {  # normal
+            "timestamps": [1, 2, 3, 5, 20],
+            "feature": _f32([10, 11, 12, 13, 14]),
+            "window": 5.0,
+            "output_feature": _f32([10.0, 10.5, 11.0, 11.5, 14.0]),
+        },
+        {  # w nan
+            "timestamps": [1, 1.5, 2, 5, 20],
+            "feature": _f32([10, nan, nan, 13, 14]),
+            "window": 1.0,
+            "output_feature": _f32([10.0, 10.0, nan, 13.0, 14.0]),
+        },
+    )
+    def test_without_sampling(
+        self,
+        timestamps,
+        feature,
+        window,
+        output_feature,
+    ):
+        evset = event_set(timestamps=timestamps, features={"a": feature})
+
+        expected_output = event_set(
+            timestamps=timestamps,
+            features={"a": output_feature},
+        )
+
+        result = evset.simple_moving_average(window_length=window)
+
+        self.assertEqual(expected_output, result)
+        result.check_same_sampling(evset)
+
+    @parameters(
+        {  # empty f32
             "timestamps": [],
             "feature": _f32([]),
             "window": 5.0,
             "sampling_timestamps": [],
             "output_feature": _f32([]),
         },
-        {  # empty f64 w sampling
+        {  # empty f64
             "timestamps": [],
             "feature": _f64([]),
             "window": 5.0,
             "sampling_timestamps": [],
             "output_feature": _f64([]),
         },
-        {  # wo sampling
-            "timestamps": [1, 2, 3, 5, 20],
-            "feature": _f32([10, 11, 12, 13, 14]),
-            "window": 5.0,
-            "output_feature": _f32([10.0, 10.5, 11.0, 11.5, 14.0]),
-        },
-        {  # w sampling
+        {  # normal
             "timestamps": [1, 2, 3, 5, 6],
             "feature": _f32([10, 11, 12, 13, 14]),
             "window": 3.0,
             "sampling_timestamps": [-1.0, 1.0, 1.1, 3.0, 3.5, 6.0, 10.0],
             "output_feature": _f32([nan, 10.0, 10.0, 11.0, 11.0, 13.5, nan]),
         },
-        {  # w nan wo sampling
-            "timestamps": [1, 1.5, 2, 5, 20],
-            "feature": _f32([10, nan, nan, 13, 14]),
-            "window": 1.0,
-            "output_feature": _f32([10.0, 10.0, nan, 13.0, 14.0]),
-        },
-        {  # w nan w sampling
+        {  # w nan
             "timestamps": [1, 2, 3, 5, 6],
             "feature": _f32([nan, 11, nan, 13, 14]),
             "window": 1.0,
             "sampling_timestamps": [1.0, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0],
             "output_feature": _f32([nan, 11.0, 11.0, nan, nan, nan, 13.0, 14]),
         },
-        {  # w variable winlen wo sampling
-            "timestamps": [0, 1, 2, 3, 5, 20],
-            "feature": _f32([nan, 10, 11, 12, 13, 14]),
-            "variable_window": _f64([1, 1, 1.5, 0.5, 3.5, 20]),
-            "output_feature": _f32([nan, 10, 10.5, 12, 12, 12]),
-        },
-        {  # w variable winlen w sampling
-            "timestamps": [0, 1, 2, 3, 5, 20],
-            "feature": [nan, 10, 11, 12, 13, 14],
-            "sampling_timestamps": [-1, 1, 4, 19, 20, 20],
-            "variable_window": [10, 0.5, 2.5, 19, 16, np.inf],
-            "output_feature": [nan, 10, 11.5, 11.5, 13.5, 12],
-        },
-        {  # w variable winlen repeated ts
-            "timestamps": [0, 1, 2, 3, 5, 20],
-            "feature": [nan, 10, 11, 12, 13, 14],
-            "sampling_timestamps": [20, 20, 20, 20, 20, 20],
-            "variable_window": [16, 0.001, np.inf, 0, 1, 19],
-            "output_feature": [13.5, 14, 12, nan, 14, 12.5],
-        },
-        {  # w variable winlen shortest duration
-            "timestamps": [1.999999, 2],
-            "feature": [10.0, 11.0],
-            "sampling_timestamps": [2, 2, 2, 2],
-            "variable_window": [1, 0.001, shortest, 0],
-            "output_feature": [10.5, 10.5, 11, nan],
-        },
-        {  # w variable winlen wo sampling invalid values
-            "timestamps": [0, 1, 2, 3, 5, 6, 20],
-            "feature": [nan, 10, 11, 12, 13, 14, 15],
-            "variable_window": [1, -20, 3, 0, 10, nan, 19],
-            "output_feature": [nan, nan, 10.5, nan, 11.5, nan, 13],
-        },
-        {  # w variable winlen w sampling invalid values
-            "timestamps": [0, 1, 2, 3, 5, 20],
-            "feature": [nan, 10, 11, 12, 13, 14],
-            "sampling_timestamps": [2, 2, 5, 5, 20, 20],
-            "variable_window": [1, -10, 3, 0, nan, 19],
-            "output_feature": [11, nan, 12.5, nan, nan, 12.5],
-        },
-        {  # wo sampling repeated ts
-            "timestamps": [0, 2, 2, 2, 2, 5],
-            "feature": [10.0, 11.0, 12.0, 13.0, 14.0, 15.0],
-            "variable_window": [1, 3, 0.5, np.inf, -1, 5],
-            "output_feature": [10, 12, 12.5, 12, nan, 13],
-        },
-        {  # w variable winlen wo sampling repeated ts same winlen
-            "timestamps": [2, 2, 2, 2],
-            "feature": _f64([10, 11, 12, 13]),
-            "variable_window": _f64([0, 1, 1, 2]),
-            "output_feature": [nan, 11.5, 11.5, 11.5],
-        },
-        {  # w variable winlen w sampling repeated ts same winlen
-            "timestamps": [0, 1, 2, 3],
-            "feature": _f64([10, 11, 12, 13]),
-            "sampling_timestamps": [2, 2, 2, 2],
-            "variable_window": _f64([0, 1, 1, 2]),
-            "output_feature": _f64([nan, 12, 12, 11.5]),
-        },
-        {  # variable winlen w sampling empty arrays
-            "timestamps": _f64([1]),
-            "feature": _f64([10]),
-            "variable_window": _f64([]),
-            "sampling_timestamps": _f64([]),
-            "output_feature": _f64([]),
-        },
-        {  # variable winlen wo sampling empty arrays
-            "timestamps": _f64([]),
-            "feature": _f64([]),
-            "variable_window": _f64([]),
-            "output_feature": _f64([]),
-        },
-        # {
-        #     "timestamps":,
-        #     "feature":,
-        #     "variable_window":,
-        #     "sampling_timestamps":,
-        #     "output_feature":,
-        # },
     )
-    def test_ok(
+    def test_with_sampling(
         self,
         timestamps,
         feature,
+        window,
+        sampling_timestamps,
         output_feature,
-        window=None,
-        variable_window=None,
-        sampling_timestamps=None,
     ):
         evset = event_set(timestamps=timestamps, features={"a": feature})
 
-        output_timestamps = timestamps
-        if sampling_timestamps is not None:
-            output_timestamps = sampling_timestamps
+        sampling = event_set(timestamps=sampling_timestamps)
 
-        if variable_window is not None:
-            window = event_set(
-                timestamps=output_timestamps,
-                features={"a": variable_window},
-            )
-
-        sampling = None
-        if variable_window is None and sampling_timestamps is not None:
-            sampling = event_set(timestamps=sampling_timestamps)
-
-        output_sampling = (
-            window
-            if variable_window is not None
-            else (sampling if sampling is not None else evset)
-        )
         expected_output = event_set(
-            timestamps=output_timestamps,
+            timestamps=sampling_timestamps,
             features={"a": output_feature},
-            same_sampling_as=output_sampling,
         )
 
         result = evset.simple_moving_average(
@@ -199,7 +122,132 @@ class SimpleMovingAverageTest(TestCase):
         )
 
         self.assertEqual(expected_output, result)
-        expected_output.check_same_sampling(result)
+        result.check_same_sampling(sampling)
+
+    @parameters(
+        {  # normal
+            "timestamps": [0, 1, 2, 3, 5, 20],
+            "feature": _f32([nan, 10, 11, 12, 13, 14]),
+            "variable_window": _f64([1, 1, 1.5, 0.5, 3.5, 20]),
+            "output_feature": _f32([nan, 10, 10.5, 12, 12, 12]),
+        },
+        {  # invalid values
+            "timestamps": [0, 1, 2, 3, 5, 6, 20],
+            "feature": [nan, 10, 11, 12, 13, 14, 15],
+            "variable_window": [1, -20, 3, 0, 10, nan, 19],
+            "output_feature": [nan, nan, 10.5, nan, 11.5, nan, 13],
+        },
+        {  # repeated ts
+            "timestamps": [0, 2, 2, 2, 2, 5],
+            "feature": [10.0, 11.0, 12.0, 13.0, 14.0, 15.0],
+            "variable_window": [1, 3, 0.5, np.inf, -1, 5],
+            "output_feature": [10, 12, 12.5, 12, nan, 13],
+        },
+        {  # repeated ts same winlen
+            "timestamps": [2, 2, 2, 2],
+            "feature": _f64([10, 11, 12, 13]),
+            "variable_window": _f64([0, 1, 1, 2]),
+            "output_feature": [nan, 11.5, 11.5, 11.5],
+        },
+        {  # empty
+            "timestamps": _f64([]),
+            "feature": _f64([]),
+            "variable_window": _f64([]),
+            "output_feature": _f64([]),
+        },
+    )
+    def test_with_variable_winlen_same_sampling(
+        self,
+        timestamps,
+        feature,
+        variable_window,
+        output_feature,
+    ):
+        evset = event_set(timestamps=timestamps, features={"a": feature})
+
+        window = event_set(
+            timestamps=timestamps,
+            features={"a": variable_window},
+        )
+
+        expected_output = event_set(
+            timestamps=timestamps,
+            features={"a": output_feature},
+        )
+
+        result = evset.simple_moving_average(window_length=window)
+
+        self.assertEqual(expected_output, result)
+        result.check_same_sampling(window)
+
+    @parameters(
+        {  # normal
+            "timestamps": [0, 1, 2, 3, 5, 20],
+            "feature": [nan, 10, 11, 12, 13, 14],
+            "window_timestamps": [-1, 1, 4, 19, 20, 20],
+            "variable_window": [10, 0.5, 2.5, 19, 16, np.inf],
+            "output_feature": [nan, 10, 11.5, 11.5, 13.5, 12],
+        },
+        {  # repeated ts
+            "timestamps": [0, 1, 2, 3, 5, 20],
+            "feature": [nan, 10, 11, 12, 13, 14],
+            "window_timestamps": [20, 20, 20, 20, 20, 20],
+            "variable_window": [16, 0.001, np.inf, 0, 1, 19],
+            "output_feature": [13.5, 14, 12, nan, 14, 12.5],
+        },
+        {  # shortest duration
+            "timestamps": [1.999999, 2],
+            "feature": [10.0, 11.0],
+            "window_timestamps": [2, 2, 2, 2],
+            "variable_window": [1, 0.001, shortest, 0],
+            "output_feature": [10.5, 10.5, 11, nan],
+        },
+        {  # invalid values
+            "timestamps": [0, 1, 2, 3, 5, 20],
+            "feature": [nan, 10, 11, 12, 13, 14],
+            "window_timestamps": [2, 2, 5, 5, 20, 20],
+            "variable_window": [1, -10, 3, 0, nan, 19],
+            "output_feature": [11, nan, 12.5, nan, nan, 12.5],
+        },
+        {  # repeated ts same winlen
+            "timestamps": [0, 1, 2, 3],
+            "feature": _f64([10, 11, 12, 13]),
+            "window_timestamps": [2, 2, 2, 2],
+            "variable_window": _f64([0, 1, 1, 2]),
+            "output_feature": _f64([nan, 12, 12, 11.5]),
+        },
+        {  # empty arrays
+            "timestamps": _f64([1]),
+            "feature": _f64([10]),
+            "variable_window": _f64([]),
+            "window_timestamps": _f64([]),
+            "output_feature": _f64([]),
+        },
+    )
+    def test_with_variable_winlen_diff_sampling(
+        self,
+        timestamps,
+        feature,
+        variable_window,
+        window_timestamps,
+        output_feature,
+    ):
+        evset = event_set(timestamps=timestamps, features={"a": feature})
+
+        window = event_set(
+            timestamps=window_timestamps,
+            features={"a": variable_window},
+        )
+
+        expected_output = event_set(
+            timestamps=window_timestamps,
+            features={"a": output_feature},
+        )
+
+        result = evset.simple_moving_average(window_length=window)
+
+        self.assertEqual(expected_output, result)
+        result.check_same_sampling(window)
 
     def test_with_index(self):
         evset = event_set(
