802e86cf8f6210572b1845389ed68a35dc9ff3f7
Ian Spektor <ispektor@tryolabs.com>
2023-10-02 19:21:49 +0200

    remove test raise

--- temporian/core/operators/window/test/test_simple_moving_average.py
+++ temporian/core/operators/window/test/test_simple_moving_average.py
@@ -58,7 +58,6 @@ class SimpleMovingAverageTest(TestCase):
         window,
         output_feature,
     ):
-        raise "a"
         evset = event_set(timestamps=timestamps, features={"a": feature})
 
         expected = event_set(
