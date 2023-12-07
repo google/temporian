224559d889e22603e197fb962ef39bf1a1206d23
javiber <javier.berneche@gmail.com>
2023-12-07 14:56:43 -0300

    Moved calendar_year to cpp

--- temporian/implementation/numpy/operators/calendar/year.py
+++ temporian/implementation/numpy/operators/calendar/year.py
@@ -20,20 +20,6 @@ from temporian.implementation.numpy.data.event_set import EventSet, IndexData
 from temporian.implementation.numpy.operators.base import OperatorImplementation
 from temporian.implementation.numpy_cc.operators import operators_cc
 
-# Copyright 2021 Google LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 class CalendarYearNumpyImplementation(OperatorImplementation):
     """Interface definition and common logic for numpy implementation of
--- temporian/implementation/numpy_cc/operators/calendar/year.cc
+++ temporian/implementation/numpy_cc/operators/calendar/year.cc
@@ -1,41 +1,49 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 
-#include <ctime>
 #include <cstdint>
+#include <ctime>
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
-#include "temporian/implementation/numpy_cc/operators/common.h"
 #include "absl/time/time.h"
+#include "temporian/implementation/numpy_cc/operators/common.h"
 namespace {
 namespace py = pybind11;
 
 absl::TimeZone handle_tz(const py::object tz) {
   if (py::isinstance<py::int_>(tz)) {
+    // handle tz as an int
     int int_tz = tz.cast<int>();
+    // tz is expresed in hours, needs to be converted to seconds
     return absl::FixedTimeZone(int_tz * 60 * 60);
+
   } else if (py::isinstance<py::float_>(tz)) {
+    // handle tz as an float
     float float_tz = tz.cast<float>();
-    float_tz = float_tz * 60 * 60;
-    return absl::FixedTimeZone(float_tz);
+    // tz is expresed in hours, needs to be converted to seconds
+    return absl::FixedTimeZone(float_tz * 60 * 60);
+
   } else if (py::isinstance<py::str>(tz)) {
+    // handle tz as an string
     std::string str_tz = tz.cast<std::string>();
     absl::TimeZone parsed_tz;
     if (!absl::LoadTimeZone(str_tz, &parsed_tz)) {
-      throw std::invalid_argument("Invalid timezone");
+      throw std::invalid_argument("Invalid timezone '" + str_tz +
+                                  "'. Only names defined in the IANA timezone" +
+                                  "database are valid");
     }
     return parsed_tz;
-    // Your C++ logic for string type
+
   } else {
     throw std::invalid_argument("Unsupported argument type");
   }
 }
 
-py::array_t<int> calendar_year(const py::array_t<double> &timestamps, const py::object tz) {
-
+py::array_t<int> calendar_year(const py::array_t<double> &timestamps,
+                               const py::object tz) {
   auto parsed_tz = handle_tz(tz);
 
   // Input size
@@ -49,16 +57,20 @@ py::array_t<int> calendar_year(const py::array_t<double> &timestamps, const py::
   auto v_timestamps = timestamps.unchecked<1>();
 
   for (Idx i = 0; i < n_events; i++) {
-    // Create tm and set the tz offset
+    // Create absolute time in UTC
     auto nanos = static_cast<int64_t>(v_timestamps[i] * 1e9);
     auto utc_time = absl::FromUnixNanos(nanos);
+
+    // Convert to civil time and get the year
     auto local_time = absl::ToCivilDay(utc_time, parsed_tz);
     v_years[i] = local_time.year();
   }
+
   return years;
-  }
-} // namespace
+}
+}  // namespace
 
 void init_calendar_year(py::module &m) {
-  m.def("calendar_year", &calendar_year, "", py::arg("timestamps").noconvert(), py::arg("tz"));
+  m.def("calendar_year", &calendar_year, "", py::arg("timestamps").noconvert(),
+        py::arg("tz"));
 }
