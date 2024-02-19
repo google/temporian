ec2785faecf87eb000f293ed1e575cf49fdf40dd
Jaedon Taylor <jaedonataylor@gmail.com>
2024-02-19 14:59:29 -0500

    Fixed max = 1 error

--- temporian/implementation/numpy/data/display_utils.py
+++ temporian/implementation/numpy/data/display_utils.py
@@ -77,31 +77,39 @@ def display_html(evset: EventSet) -> str:
         index_data = evset.data[index_key]
         num_timestamps = len(index_data.timestamps)
         max_timestamps = min(config.display_max_events, num_timestamps)
-        # Slices timestamps and features if there are more than 'max_timestamps' events
-        half_max_timestamps = max_timestamps // 2
-        display_timestamps = (
-            np.concatenate(
-                (
-                    index_data.timestamps[:half_max_timestamps],
-                    index_data.timestamps[-half_max_timestamps:],
-                )
-            )
-            if num_timestamps > max_timestamps
-            else index_data.timestamps
-        )
-        display_features = (
-            [
+        if max_timestamps == 1:
+            display_timestamps = index_data.timestamps[
+                :1
+            ]  # Just take the first timestamp
+            display_features = [
+                values[:1] for values in index_data.features
+            ]  # And the corresponding features
+        else:
+            # Slices timestamps and features if there are more than 'max_timestamps' events
+            half_max_timestamps = max_timestamps // 2
+            display_timestamps = (
                 np.concatenate(
                     (
-                        values[:half_max_timestamps],
-                        values[-half_max_timestamps:],
+                        index_data.timestamps[:half_max_timestamps],
+                        index_data.timestamps[-half_max_timestamps:],
                     )
                 )
-                for values in index_data.features
-            ]
-            if num_timestamps > max_timestamps
-            else index_data.features
-        )
+                if num_timestamps > max_timestamps
+                else index_data.timestamps
+            )
+            display_features = (
+                [
+                    np.concatenate(
+                        (
+                            values[:half_max_timestamps],
+                            values[-half_max_timestamps:],
+                        )
+                    )
+                    for values in index_data.features
+                ]
+                if num_timestamps > max_timestamps
+                else index_data.features
+            )
 
         # Display index values
         html_index_value = html_div(dom)
@@ -175,9 +183,9 @@ def display_html(evset: EventSet) -> str:
             # Create ellipsis row between first half and last half if more than man_timestamps entries
             table.appendChild(html_table_row(dom, row))
             if (
-                timestamp_idx == (half_max_timestamps - 1)
+                timestamp_idx == ((max_timestamps // 2) - 1)
                 and num_timestamps > max_timestamps
-            ):
+            ) or (max_timestamps == 1):
                 ellipsis_row = [ELLIPSIS] * (
                     1 + len(visible_feats) + int(has_hidden_feats)
                 )
